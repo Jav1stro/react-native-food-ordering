b78fe431a87a0528c073224f0f5f9dd46e82598e
Javier Amarilla <javier.amarilla@casademoneda.gob.ar>
2024-04-12 12:15:45 -0300

    Added typescript config paths

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, View, Image } from "react-native";
-import Colors from "../constants/Colors";
+import Colors from "@constants/Colors";
 import { Product } from "../types/types";
 
 type ProductListItemProps = {
